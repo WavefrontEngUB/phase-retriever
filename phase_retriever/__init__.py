993b1b16c82918af4011c537a607409d3825eab0
David Maluenda Niubó <dmaluenda@ub.edu>
2023-01-16 10:51:31 +0100

    commenting out the GUI dependency in the main program

--- phase_retriever/__init__.py
+++ phase_retriever/__init__.py
@@ -1,3 +1,5 @@
-from .interface import PhaseRetrieverGUI 
-from .wx_gui import wxGUI
+# GUI should be separate from the main program to avoid wx dependency in command line mode.
+# from .interface import PhaseRetrieverGUI
+# from .wx_gui import wxGUI
+
 from .retriever import PhaseRetriever
