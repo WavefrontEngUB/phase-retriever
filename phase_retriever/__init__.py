c3dfb2c31bd0942eb7f0cea1949f46c71e2b2ab8
David Maluenda Niubó <dmaluendan@gmail.com>
2024-03-11 16:16:46 +0100

    Update __init__.py

--- phase_retriever/__init__.py
+++ phase_retriever/__init__.py
@@ -1,5 +1,5 @@
 # GUI should be separate from the main program to avoid wx dependency in command line mode.
-# from .interface import PhaseRetrieverGUI
-# from .wx_gui import wxGUI
+from .interface import PhaseRetrieverGUI
+from .wx_gui import wxGUI
 
 from .retriever import PhaseRetriever
